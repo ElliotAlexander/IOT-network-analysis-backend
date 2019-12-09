c6612250467faae7b5b4d6c0578a8cce11f69e21
Elliot Alexander <ep1e16@soton.ac.uk>
2019-12-09 11:16:34 +0000

    Improved Network Identification Testing:

--- src/test/java/NetworkIdentificationTest.java
+++ src/test/java/NetworkIdentificationTest.java
@@ -10,7 +10,34 @@ public class NetworkIdentificationTest {
     @Test
     public void sameSubnetTest(){
         try {
-            NetworkIdentification.compareIPSubnets(InetAddress.getByName("127.0.0.1").getAddress(), InetAddress.getByName("127.0.0.55").getAddress(), InetAddress.getByName("255.255.255.0").getAddress());
+            assert NetworkIdentification.compareIPSubnets(InetAddress.getByName("127.0.0.1").getAddress(), InetAddress.getByName("127.0.0.55").getAddress(), InetAddress.getByName("255.255.255.0").getAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void differentSubnetTests(){
+        try {
+            assert !NetworkIdentification.compareIPSubnets(InetAddress.getByName("128.0.0.1").getAddress(), InetAddress.getByName("127.0.0.55").getAddress(), InetAddress.getByName("255.255.255.0").getAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void erroredSubnetTest(){
+        try {
+
+            byte[] one = InetAddress.getByName("127.0.0.1").getAddress();
+            byte[] two = { 0x55 };
+            byte[] combined = new byte[one.length + two.length];
+
+            System.arraycopy(one,0,combined,0         ,one.length);
+            System.arraycopy(two,0,combined,one.length,two.length);
+            assert !NetworkIdentification.compareIPSubnets( combined, InetAddress.getByName("127.0.0.55").getAddress(), InetAddress.getByName("255.255.255.0").getAddress());
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
